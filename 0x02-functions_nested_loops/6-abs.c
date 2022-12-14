5430edeca9627cad05a434c9b5b1491d3b315756
Ntim1234 <macdumanlegacy@gmail.com>
2022-12-13 18:23:41 -0800

    task 3 to 8

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @t: the integer to be printed
+ * Return: The absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
